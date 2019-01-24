7d749bd22433b76136c43df0403d16e792046704
StaleLoafOfBread <gandrew111@gmail.com>
2019-01-23 22:41:38 -0500

    refactor(login): separate out form WIP

---
+++ components/forms/LoginForm.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, KeyboardAvoidingView } from "react-native";
+
+export default class LoginForm extends React.Component {
+    render(){
+        return (
+            <KeyboardAvoidingView  behavior="padding" style={styles.container}>
+                <TextInput style={styles.input}
+                    autoCapitalize="none"
+                    onSubmitEditing={() => this.passwordInput.focus()}
+                    autoCorrect={false}
+                    keyboardType='email-address'
+                    returnKeyType="next"
+                    placeholder='Username'
+                    placeholderTextColor='rgba(225,225,225,0.7)' />
+
+                <TextInput style={styles.input}
+                    returnKeyType="go"
+                    ref={(input) => this.passwordInput = input}
+                    placeholder='Password'
+                    placeholderTextColor='rgba(225,225,225,0.7)'
+                    secureTextEntry />
+
+                <TouchableOpacity style={styles.buttonContainer}>
+                    <Text style={styles.buttonText} onPress={this._signInAsync}>Login</Text>
+                </TouchableOpacity>
+            </KeyboardAvoidingView>
+        );
+    }
+};
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    padding: 35
+  },
+  input: {
+    height: 40,
+    width: 300,
+    backgroundColor: "rgba(225,225,225,0.3)",
+    marginBottom: 10,
+    padding: 10,
+    color: "#fff",
+    borderRadius: 20
+  },
+  buttonContainer: {
+    width: 300,
+    backgroundColor: "#000000",
+    paddingVertical: 15,
+    borderRadius: 20
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "700"
+  }
+});
--- screens/AuthLoginScreen.js
+++ screens/AuthLoginScreen.js
@@ -1,45 +1,34 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text, View, Image, KeyboardAvoidingView, TouchableOpacity, AsyncStorage, TextInput } from "react-native";
+import { ScrollView, StyleSheet, Text, View, Image, TouchableOpacity, AsyncStorage, TextInput } from "react-native";
 
 import theme from '../constants/Colors';
+import LoginForm from '../components/forms/LoginForm';
+
 
 export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this._signInAsync = this._signInAsync.bind(this);
+      }
+    
     static navigationOptions = {
         title: 'Login',
     };
+
     _signInAsync = async () => {
+        console.log('foobar');
         await AsyncStorage.setItem('userToken', 'abc');
         this.props.navigation.navigate('AuthLoading');
       };
-    
+
     render() {
         return (
-            <KeyboardAvoidingView  behavior="padding" style={styles.container}>
+            <View style={styles.container}>
                 <View style={styles.logoContainer}>
                     <Image resizeMode="contain" style={styles.logo} source={require('../assets/images/d-logo-white.png')}/>                    
                 </View>
-                <View style={styles.inputContainer}>
-                    <TextInput style={styles.input}
-                        autoCapitalize="none"
-                        onSubmitEditing={() => this.passwordInput.focus()}
-                        autoCorrect={false}
-                        keyboardType='email-address'
-                        returnKeyType="next"
-                        placeholder='Username'
-                        placeholderTextColor='rgba(225,225,225,0.7)' />
-
-                    <TextInput style={styles.input}
-                        returnKeyType="go"
-                        ref={(input) => this.passwordInput = input}
-                        placeholder='Password'
-                        placeholderTextColor='rgba(225,225,225,0.7)'
-                        secureTextEntry />
-
-                    <TouchableOpacity style={styles.buttonContainer}>
-                        <Text style={styles.buttonText} onPress={this._signInAsync}>Login</Text>
-                    </TouchableOpacity>
-                </View>
-            </KeyboardAvoidingView>
+                <LoginForm />
+            </View>
             );
         }
     }
@@ -62,28 +51,5 @@ const styles = StyleSheet.create({
         position: 'absolute',
         width: 600,
         height: 200
-    },
-    inputContainer: {
-        padding: 35
-    },
-      input: {
-        height: 40,
-        width: 300,
-        backgroundColor: "rgba(225,225,225,0.3)",
-        marginBottom: 10,
-        padding: 10,
-        color: "#fff",
-        borderRadius: 20
-      },
-      buttonContainer: {
-        width: 300,
-        backgroundColor: "#000000",
-        paddingVertical: 15,
-        borderRadius: 20
-      },
-      buttonText: {
-        color: "#fff",
-        textAlign: "center",
-        fontWeight: "700"
-      }
+    }
 });
